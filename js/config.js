de32d12dabc50a1eeb9663c0c409b55acffec30c
PomeloYolo <pomelo.yolo@gmail.com>
2025-08-28 16:46:52 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@ const PASSWORD_CONFIG = {
 const SITE_CONFIG = {
     name: 'LibreTV',
     url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    description: '免費線上影視觀看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
