09d000243c5fa8ce050fb20b73bcdb196e2c5526
PomeloYolo <pomelo.yolo@gmail.com>
2025-08-29 20:33:30 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,7 @@
 const CUSTOMER_SITES = {
     qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七資源',
+        api: 'https://cj.lziapi.com/api.php/provide/vod',
+        name: '非凡資源',
     }
 };
 
